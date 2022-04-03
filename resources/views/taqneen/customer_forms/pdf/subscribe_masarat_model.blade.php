1e7efe8c8d4bd82b35eeab5c6f7e136fde63cc20
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-03 16:34:00 +0200

    fsdf

--- resources/views/taqneen/customer_forms/pdf/subscribe_masarat_model.blade.php
+++ resources/views/taqneen/customer_forms/pdf/subscribe_masarat_model.blade.php
@@ -8,26 +8,68 @@
     <title>TamUser</title>
     @include('taqneen.customer_forms.pdf.common_style')
   
-<body style="background-image: url('{{ url('/assets/images/masarat-pdf/page.jpg') }}');background-size: cover;background-repeat: no-repeat;" >
-    <img style="height: 26.458333333cm" src="{{ url('/assets/images/masarat-pdf/page.jpg') }}" class="pdf_image w3-display-topleft"alt="">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.debug.js" ></script>
+    <style>
+        body {
+            background-color: #fafafa;
+        }
     
-    <!-- ////////////////////////////////بيانات المنشأة/////////////////////////////////// -->
-    @foreach ($data as $key => $value)
-    @php
-        $opt = $options[$key]?? []; 
-        $style = "position: fixed;";
+        .pdf-viewer {
+            width: 210mm;
+            height: 297mm;
+            margin: auto;
+            background-image: url('{{ url('/assets/images/masarat-pdf/page.jpg') }}');
+            background-size: contain;
+            background-repeat: no-repeat;
+            position: relative;
+        }
+    
+    </style>
+</head>
+<body _style="background-image: url('{{ url('/assets/images/masarat-pdf/page.jpg') }}');background-size: cover;background-repeat: no-repeat;" >
+    <br>
 
-        $style .= isset($opt['left'])? "left:" . $opt['left'] . ";" : ''; 
-        $style .= isset($opt['top'])? "top:" . $opt['top'] . ";" : ''; 
-        $style .= isset($opt['fontSize'])? "font-size:" . $opt['fontSize'] . ";" : ''; 
-        $style .= isset($opt['letterSpacing'])? "letter-spacing:" . $opt['letterSpacing'] . ";" : ''; 
- 
-    @endphp
-    <div style="{{ $style }}" >
-        {{ is_array($value)? '' : $value }}
+    <div class="pdf-viewer w3-card w3-white" id="page" >
+        @foreach ($data as $key => $value)
+        @php
+            $opt = $options[$key]?? []; 
+            $style = "position: fixed;";
+    
+            $style .= isset($opt['left'])? "left:" . $opt['left'] . ";" : ''; 
+            $style .= isset($opt['top'])? "top:" . $opt['top'] . ";" : ''; 
+            $style .= isset($opt['fontSize'])? "font-size:" . $opt['fontSize'] . ";" : ''; 
+            $style .= isset($opt['letterSpacing'])? "letter-spacing:" . $opt['letterSpacing'] . ";" : ''; 
+     
+        @endphp
+        <div style="{{ $style }}" >
+            {{ is_array($value)? '' : $value }}
+        </div>
+        @endforeach
     </div>
-    @endforeach
+    <br>
+  
+    
  
+    <script>
+        function generatePDF() {
+            var doc = new jsPDF();
+             doc.fromHTML(document.getElementById("page"), // page element which you want to print as PDF
+             15,
+             15, 
+             {
+               'width': 170
+             },
+             function(a) 
+              {
+               doc.save("HTML2PDF.pdf");
+             });
+           }
+
+
+        window.onload = function(){
+            generatePDF();
+        }
+    </script>
       
 </body>
 </html>
