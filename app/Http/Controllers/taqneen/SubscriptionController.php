aa5b6b07da3f9b75d7c6e4d0829f91242050c732
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-14 22:18:24 +0200

    edtis of opportunities

--- app/Http/Controllers/taqneen/SubscriptionController.php
+++ app/Http/Controllers/taqneen/SubscriptionController.php
@@ -34,14 +34,9 @@ class SubscriptionController extends Controller
 
         $business_id = session('business.id');
         $services = Category::where("business_id", $business_id)->where('category_type', 'service')->get(); 
-        
-        $id = 41;
-        $role = Role::find(41);
-
-        $users = $role->users()->get();
-
-        dd($users);
-        $users = User::where('user_type','user')->where('business_id', session('business.id'))->latest()->get()->pluck('user_full_name', 'id')->toArray();
+         
+        $role = Role::find(41);  
+        $users = $role->users()->get()->pluck('user_full_name', 'id')->toArray();
         $payment_status = [
             "paid" => __('paid'),
             "not_paid" => __('not_paid')
