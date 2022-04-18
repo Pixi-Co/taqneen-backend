9ad8466aa213c8e38fd87d64098f69a32483c743
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-18 06:44:09 +0200

    edit of reports

--- resources/views/taqneen/layouts/script.blade.php
+++ resources/views/taqneen/layouts/script.blade.php
@@ -32,10 +32,10 @@
 <script src="{{ asset('js/iziToast.js') }}"></script>
 <script src="{{ asset('js/fullcalendar.min.js') }}"></script>
 <script src="{{ asset('assets/js/theme-customizer/customizer.js') }}"></script>
- 
+     
 @if (optional(auth()->user()->roles()->first())->name == 'customer#19' || !auth()->user()->roles()->first())
 <script src="{{ url('/') }}/supportboard/js/min/jquery.min.js"></script>
-<script id="sbinit" src="{{  url('/')  }}/supportboard/js/main.js"></script>
+<script id="sbinit" src="{{  url('/')  }}/supportboard/js/main.js"></script> 
 @endif
 
 <script>
