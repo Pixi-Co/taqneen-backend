32935a04c4b8aa6c315edb66f5f3ddda2fa6a769
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-21 04:33:43 +0200

    edits

--- app/Http/Controllers/taqneen/SubscriptionController.php
+++ app/Http/Controllers/taqneen/SubscriptionController.php
@@ -149,6 +149,7 @@ class SubscriptionController extends Controller
                     "paid" => __('paid'),
                     "not_paid" => __('not_paid')
                 ];
+                $row->payment = TransactionPayment::where('transaction_id', $row->id)->latest()->first();
                 return view('taqneen.subscription.actions', compact('row', 'payment_methods', 'payment_status'));
             })  
             ->editColumn('created_by', function ($row) {
