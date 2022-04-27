e13bedc4b4f2b9f228a12819eb425546c6c38b9a
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-27 02:22:51 +0200

    edits

--- resources/views/taqneen/customers/index.blade.php
+++ resources/views/taqneen/customers/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('title', 'Default')
 
+
 @section('css')
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/animate.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/chartist.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/date-picker.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/datatables.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/datatable-extension.css') }}">
 @endsection
 
 @section('style')
@@ -177,6 +179,25 @@ var session_layout = '{{ session()->get('layout') }}';
 <script src="{{asset('assets/js/typeahead-search/typeahead-custom.js')}}"></script>
 <script src="{{asset('assets/js/datatable/datatables/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('assets/js/datatable/datatables/datatable.custom.js')}}"></script>
+
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.buttons.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/jszip.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/buttons.colVis.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/pdfmake.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/vfs_fonts.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.autoFill.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.select.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/buttons.bootstrap4.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/buttons.html5.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/buttons.print.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.bootstrap4.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.responsive.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/responsive.bootstrap4.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.keyTable.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.colReorder.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.fixedHeader.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.rowReorder.min.js"></script>
+<script src="{{ url('/assets') }}/js/datatable/datatable-extension/dataTables.scroller.min.js"></script>
 
 
 <script>
