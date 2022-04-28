85d9942516e431496dd426c542982e31765cd2c1
alifaragmahmed <alifaragmahmed@gmail.com>
2022-04-28 05:30:27 +0200

    edits

--- app/Http/Controllers/taqneen/ReportController.php
+++ app/Http/Controllers/taqneen/ReportController.php
@@ -193,7 +193,6 @@ class ReportController extends Controller
 
     public function subscriptions()
     {
-        dd(Subscription::where('is_expire', 1)->get()->toArray());
         if (request()->ajax()) {
             return $this->dataOfSubscription();
         }
@@ -201,6 +200,7 @@ class ReportController extends Controller
         $business_id = session('business.id');
         $services = Category::where("business_id", $business_id)->where('category_type', 'service')->get();
 
+        dd(Subscription::where("business_id", $business_id)->where('is_expire', 1)->get()->toArray());
         $expire_date = date('Y-m-d', strtotime(date('Y-m-d'). ' + 31 days'));
         $data = [
             'subscription_total' => Transaction::where('business_id', session('business.id'))->sum('final_total'),
